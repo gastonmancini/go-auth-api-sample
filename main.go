077583da6be3c82b8f7eb5bec26d06e02170ecfb
Gaston Mancini <gastonmancini@gmail.com>
2021-06-09 13:30:19 -0300

    Add CORS middleware

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"go-auth-api-sample/routes"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
@@ -12,6 +13,11 @@ func main() {
 	database.Connect()
 
 	app := fiber.New()
+
+	// Add CORS Middleware so the frontend get the cookie
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
 
 	routes.Setup(app)
 
